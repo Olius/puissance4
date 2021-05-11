516361661ecbcde79347583f4329c9dd5aa1d0fa
Louis de Forcrand <olius@bluewin.ch>
2021-05-11 18:57:56 +0200

    Switch to ZipList

--- Board.hs
+++ Board.hs
@@ -1,20 +1,21 @@
 module Board where
 
 import State
+import Control.Applicative
 
 type Chip = Player
 type Row = [Chip]
 type Col = [Chip]
-newtype Board = Board { getBoard :: [Col] }
+newtype Board = Board { getBoard :: ZipList Col }
         deriving Show
 
 instance Semigroup Board where
-        (Board cs) <> (Board ds) = Board $ zipWith (++) cs ds
+        (Board cs) <> (Board ds) = Board $ liftA2 (<>) cs ds
 instance Monoid Board where
-        mempty = Board $ repeat []
+        mempty = Board $ ZipList $ repeat []
 
 move2Board :: Move -> Board
-move2Board (p,c) = Board $ replicate c mempty ++ [[p]] ++ repeat mempty
+move2Board (p,c) = Board $ ZipList $ replicate c mempty ++ [[p]] ++ repeat mempty
 
 state2Board :: State -> Board
 state2Board = foldMap move2Board
