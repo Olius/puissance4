03f454530f4e420fa5ba861fbfd0fabc7ae0a56e
Louis de Forcrand <olius@bluewin.ch>
2021-05-11 18:58:19 +0200

    Add bounded-board tree

--- PTree.hs
+++ PTree.hs
@@ -9,3 +9,6 @@ allMoves = [ (p,c) | p <- [minBound..], c <- [0..] ]
 
 fullTree :: Forest Move
 fullTree = unfoldForest (\m -> (m, allMoves)) allMoves
+
+nTree :: ICol -> Forest Move
+nTree n = [ Node (p,c) $ nTree n | p <- [minBound..] , c <- [0..n-1] ]
